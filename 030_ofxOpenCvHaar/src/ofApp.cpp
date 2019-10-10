88ae9c92c8c6e8dfa19cdf13861d068dad4ca422
Lewis Lepton <lewisleptondev@gmail.com>
2019-10-10 10:30:00 +0100

    added '030_ofxOpenCvHaar'

---
+++ 030_ofxOpenCvHaar/src/ofApp.cpp
@@ -0,0 +1,89 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+  camera.setup(640, 480);
+  
+  color.allocate(camera.getWidth(), camera.getHeight());
+  gray.allocate(camera.getWidth(), camera.getHeight());
+  
+  haar.setup("haarcascade_frontalface_default.xml");
+  haar.setScaleHaar(3);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+  camera.update();
+  
+  if (camera.isFrameNew()){
+    color.setFromPixels(camera.getPixels());
+    gray = color;
+    haar.findHaarObjects(gray);
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+  color.draw(0, 0);
+  
+  for (int i = 0; i < haar.blobs.size(); i++) {
+    ofSetColor(255);
+    ofNoFill();
+    ofDrawRectangle(haar.blobs[i].boundingRect);
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
