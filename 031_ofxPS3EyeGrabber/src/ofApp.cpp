bc442357356a711010be5d83ec89949d1505a66c
Lewis Lepton <lewisleptondev@gmail.com>
2019-10-15 10:30:37 +0100

    added  '031_ofxPS3EyeGrabber'

---
+++ 031_ofxPS3EyeGrabber/src/ofApp.cpp
@@ -0,0 +1,72 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+  camera.setGrabber(std::make_shared<ofxPS3EyeGrabber>());
+  camera.setup(640, 480);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+  camera.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+  camera.draw(0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
