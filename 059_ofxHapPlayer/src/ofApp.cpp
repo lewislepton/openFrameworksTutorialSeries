e3b9b9ef8c46fa9afe285532912ae4beabe193c9
Lewis Lepton <lewisleptondev@gmail.com>
2020-01-21 09:43:54 +0000

    added '059_ofxHapPlayer'

---
+++ 059_ofxHapPlayer/src/ofApp.cpp
@@ -0,0 +1,73 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+  video.load("video.mov");
+  video.play();
+  video.setLoopState(OF_LOOP_PALINDROME);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+  video.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+  video.draw(0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
